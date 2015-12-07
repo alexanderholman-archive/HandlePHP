3a68ce935203870516efab8172b64aeb5233b5b3
Alexander Holman <alex@evosite.co.uk>
2015-12-07 18:12:12 +0000

    rename magicMethod to magic_method

--- require.php
+++ require.php
@@ -6,11 +6,15 @@ $defaultSettings = require_once ( 'config/default.php' );
 
 $settings = ( isset( $settings ) ) ? array_merge_recursive( $defaultSettings, $settings ) : $defaultSettings;
 
-require_once( './trait/magicMethods/_base.php' );
+require_once( './trait/magic_methods/_base.php' );
 
 /**
  * Class php
  * @property php\database $database
  * @method php\database database
  */
-class php { use magicMethods; }
+class php {
+
+    use magic_methods;
+
+}
